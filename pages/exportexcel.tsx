6793da6089a9e39ba882c4b84a8ddacdd5a4a478
sugayutokyo <mygit_yutasugao20171126@yahoo.co.jp>
2022-10-07 16:10:41 +0900

    fix design

--- pages/exportexcel.tsx
+++ pages/exportexcel.tsx
@@ -12,40 +12,94 @@ const exportexcel: NextPage<{ liff: Liff | null; liffError: string | null }> = (
     e.preventDefault();
     // Workbookの作成
     const workbook = new ExcelJS.Workbook();
+    workbook.creator = 'Sugayutokyo';
+    workbook.created = new Date(1985, 8, 30);
     // Workbookに新しいWorksheetを追加
     workbook.addWorksheet('My Sheet');
     // ↑で追加したWorksheetを参照し変数に代入
     const worksheet = workbook.getWorksheet('My Sheet');
-    // 列を定義
-    worksheet.columns = [
-      { header: 'ID', key: 'id' },
-      { header: '名称', key: 'name' },
-      { header: '価格', key: 'price' },
-    ];
-    // 行を定義
-    worksheet.addRow({id: 1001, name: 'みかん', price: 170});
-    worksheet.addRow({id: 1002, name: 'バナナ', price: 200});
-    worksheet.addRow({id: 1003, name: 'りんご', price: 260});
-    worksheet.addRow({id: 1004, name: 'トマト', price: 190});
+    // カラムのwidthを全て2にする
+    const numToAdustColumn = 100; // widthを調整したいカラム数
+    for (let i = 1; i < numToAdustColumn; i++) {
+      const nameCol = worksheet.getColumn(i);
+      nameCol.width = 14;
+    }
+    worksheet.pageSetup.paperSize = 8; // A3 size
+
+    // ヘッダー
+    worksheet.getCell("B2").font = {
+      name: 'MSゴシック',
+      size: 16,
+      bold: true
+    };
+    worksheet.getCell("B2").value = "工事・安全衛生日誌 兼 環境・品質日誌";
+    worksheet.getCell("CQ2").value = "天候";
+    worksheet.getCell("CQ3").value = "晴れ";
+
+    worksheet.getCell("F4").font = {
+      name: 'MSゴシック',
+      size: 14
+    };
+    worksheet.mergeCells('B4:H4');
+    worksheet.mergeCells('J4:K4');
+    worksheet.getCell('B4').alignment = { vertical: 'middle', horizontal: 'center' };
+    worksheet.getCell('B6').alignment = { vertical: 'middle', horizontal: 'center' };
+    worksheet.getCell("B4").value = "前日記入";
+
+    worksheet.getCell('C4', 'C6').border = {
+      top: {style:'thin'},
+      left: {style:'thin'},
+      bottom: {style:'thin'},
+      right: {style:'thin'}
+    };
+
+    worksheet.addTable({
+      name: '前日記入',
+      ref: 'B5',
+      headerRow: true,
+      // style: {
+      //   theme: 'TableStyleDark3',
+      //   showRowStripes: true,
+      // },
+      columns: [
+        {name: '業者名'},
+        {name: '受理者'},
+        {name: '予定人員'},
+        {name: 'No.'},
+        {name: '予定工事'},
+        {name: '責任者'},
+        {name: '名称'},
+        {name: '選任・資格者'},
+        {name: '手順No'},
+        {name: '安全衛生指示事項'},
+      ],
+      rows: [
+        [
+          '千代田建設', // 業者名
+          '田中', // 受理者
+          5, // 予定人員
+          1, // No.
+          'Ａ工区5階部外部足場組み立て', // 予定工事
+          '田中', // 責任者
+          '主', // 名称
+          '田中', // 選任・資格者
+          1, // 手順No
+          '板付き布枠に乗って枠を運び組立てるとき\nバランスを崩して墜落しない様に\n親綱を先行設置し安全帯を確実に使用の事' // 安全衛生指示事項
+        ],
+      ],
+    });
     // UInt8Arrayを生成
     const uint8Array = await workbook.xlsx.writeBuffer();
     // Blob
     const blob = new Blob([uint8Array], {type: 'application/octet-binary'});
     const url = window.URL.createObjectURL(blob);
     setCreatedUrl(url);
-    // const a = document.createElement('a');
-    // a.href = url;
-    // a.download = `sample.xlsx`;
-    // a.click();
-    // // ダウンロード後は不要なのでaタグを除去
-    // a.remove()
-    if (liff) {
-      liff.openWindow({
-        // url: url,
-        url: 'https://www.google.co.jp/',
-        external: true,
-      });
-    }
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = `sample.xlsx`;
+    a.click();
+    // ダウンロード後は不要なのでaタグを除去
+    a.remove()
   } 
   return (
     <div>
