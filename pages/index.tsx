72311bfa472ca4033282911496eb991b8a5ad457
sugayutokyo <mygit_yutasugao20171126@yahoo.co.jp>
2022-10-24 15:34:26 +0900

    add

--- pages/index.tsx
+++ pages/index.tsx
@@ -29,9 +29,29 @@ const Home: NextPage<{ liff: Liff | null; liffError: string | null }> = ({
   }
 
   const getLocation = async () => {
-    navigator.geolocation.getCurrentPosition((position) => {
-      alert("緯度:" + position.coords.latitude+",経度"+ position.coords.longitude);
-    });
+    navigator.geolocation.getCurrentPosition(
+      // 取得成功した場合
+      (position) => {
+        alert("緯度:" + position.coords.latitude+",経度"+ position.coords.longitude);
+      },
+      // 取得失敗した場合
+      function(error) {
+        switch(error.code) {
+          case 1: //PERMISSION_DENIED
+            alert("位置情報の利用が許可されていません");
+            break;
+          case 2: //POSITION_UNAVAILABLE
+            alert("現在位置が取得できませんでした");
+            break;
+          case 3: //TIMEOUT
+            alert("タイムアウトになりました");
+            break;
+          default:
+            alert("その他のエラー(エラーコード:"+error.code+")");
+            break;
+        }
+      }
+    );
   }
 
   const saveImage = async () => {
@@ -128,6 +148,9 @@ const Home: NextPage<{ liff: Liff | null; liffError: string | null }> = ({
         <button onClick={getLocation} className={styles.button}>位置情報を取得する</button>
         <br/>
         <button onClick={getIndex} className={styles.button}>一覧を取得する</button>
+
+        <a href="">友達を追加する</a>
+        <br/>
         {index.length > 0 && (
           <>
             <table border={1} className={styles.table_contents}>
