6e835fc5fb162f67ea1eaf26bc929a4bc4c77f42
sugayutokyo <mygit_yutasugao20171126@yahoo.co.jp>
2022-11-28 17:38:46 +0900

    get id

--- pages/index.tsx
+++ pages/index.tsx
@@ -60,8 +60,8 @@ const Home: NextPage<{ liff: Liff | null; liffError: string | null }> = ({
       'type': 'text',
       'text': 'Hello, World! from SHare Target Picker App...'
     }]).then(() => {
-
-      alert(liff);
+      alert(liff.getAId());
+      alert(liff.id);
     });
   }
 
